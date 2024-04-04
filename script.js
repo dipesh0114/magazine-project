4e77cc7c3690239ca7fefdd947ec82efb3c73009
Dipesh <dipeshpatro@gmail.com>
2024-04-04 12:07:27 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+function changeBg(){
+    const images = [
+        'url("7.jpg")',
+        'url("3.jpg")',
+        'url("6.jpg")',
+        'url("2.jpg")',
+    ]
+
+    const home = document.getElementById("home")
+    const bg = images[Math.floor(Math.random()*images.length)];
+    home.style.background = bg;
+}
+
+setInterval(changeBg, 1000)
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var message = document.getElementById("message");
+
+    function isInViewport(element) {
+        var bounding = element.getBoundingClientRect();
+        return (
+            bounding.top >= 0 &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        );
+    }
+
+    function showOnScroll() {
+        if (isInViewport(message)) {
+            message.style.opacity = "1";
+            message.style.transform = "translateY(0)";
+            window.removeEventListener("scroll", showOnScroll);
+        }
+    }
+
+    window.addEventListener("scroll", showOnScroll);
+    showOnScroll(); // Check on initial load
+});
